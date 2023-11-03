731fc3c18fe57027a8c1607420b840d173f3e669
Mike Setzer <michaelsetzerucf@gmail.com>
2023-11-03 10:50:19 -0400

    edits

--- template-contact.php
+++ template-contact.php
@@ -4,7 +4,7 @@
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		<main class="contact">
+		<main class="general">
 
             <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
             <section class="page-header" style="background: url(<?php echo $thumb[0]; ?>) no-repeat center center; background-size: cover;">
