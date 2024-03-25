6d2b040dcea7e050932bb429467f5bf32edc1cb9
mi115102 <michael.setzer@ucf.edu>
2024-03-25 12:35:39 -0400

    Contact Fix

--- template-contact.php
+++ template-contact.php
@@ -6,39 +6,11 @@
 
 		<main class="contact">
 
-			<img id="mapPrint" src="<?php bloginfo('template_directory'); ?>/library/images/print-map.png" alt="Printable Map" class="hide" />
-		
-			<section id="fullMap">
-
-				<!-- <a class="printMap" href="#">Print Map</a> -->
-
-				<div id="map-side"></div>
-
-			</section>
-
 			<section class="contactStuff">
 
-				<div class="left-side">	
+				<div class="left-side">
 
 					<div class="top-part">
-						
-						<?php/* if ( !get_field( 'time_message' ) ) { ?>
-							<h3>We are currently:</h3>
-						<?php } else { ?>
-							<?php the_field( 'time_message' ); ?>
-						<?php } ?>
-
-						<h3>
-							<?php 
-								$timeNow = date( 'H:i:s' ); 
-								$open = date( 'w' ) == 0 || date( 'w' ) == 6 ? FALSE : TRUE;
-								if ( $timeNow >= get_field( 'open_time' ) && $timeNow <= get_field( 'close_time' ) && $open ) { ?>
-									<span class="color-green">OPEN</span> 
-							<?php } else { ?>
-								<span class="color-red">CLOSED</span> 
-							<?php } ?>
-							<?php echo '- ' . date( 'h:i a', strtotime( get_field( 'open_time' ) ) ) . ' to ' . date( 'h:i a', strtotime( get_field( 'close_time' ) ) ); ?>
-						</h3> */ ?>
 
 						<ul class="infoList">
 
@@ -55,7 +27,7 @@
 							</li>
 
 							<?php } ?>
-							
+
 							<?php if ( get_field( 'address' ) ) { ?>
 
 								<li>
@@ -69,7 +41,7 @@
 								</li>
 
 							<?php } ?>
-							
+
 							<?php if ( get_field( 'contact_info' ) ) { ?>
 
 								<li>
@@ -83,7 +55,7 @@
 								</li>
 
 							<?php } ?>
-							
+
 							<?php if ( get_field( 'hours_of_operation' ) ) { ?>
 
 								<li>
@@ -97,19 +69,19 @@
 								</li>
 
 							<?php } ?>
-							
+
 							<?php if ( get_field( 'facebook_link' ) || get_field( 'linkedin_link' ) || get_field( 'twitter_link' ) ) { ?>
 
 								<li>
-									
+
 									<?php if ( get_field( 'facebook_link' ) ) { ?>
 										<a href="<?php the_field( 'facebook_link' ); ?>" class="contact-icon fb first"></a>
 									<?php } ?>
-									
+
 									<?php if ( get_field( 'twitter_link' ) ) { ?>
 										<a href="<?php the_field( 'twitter_link' ); ?>" class="contact-icon tw"></a>
 									<?php } ?>
-									
+
 									<?php if ( get_field( 'linkedin_link' ) ) { ?>
 										<a href="<?php the_field( 'linkedin_link' ); ?>" class="contact-icon linkedin"></a>
 									<?php } ?>
@@ -139,11 +111,11 @@
 				</div>
 
 				<div class="right-side">
-					
+
 					<?php the_content(); ?>
-					
+
 				</div>
-				
+
 				<div class="clear"></div>
 
 			</section>
